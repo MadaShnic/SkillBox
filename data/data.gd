4f486fa1c9cfec30b7ea0470a87b6456bd87e111
MadaShnic <lukaculo1234@gmail.com>
2025-12-18 17:31:09 +0100

    data structure and base script

---
+++ data/data.gd
@@ -0,0 +1,280 @@
+extends Node
+
+const excercises := {
+	"anxiety": {
+		"short": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		},
+		"long": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		}
+	},
+	"grief": {
+		"short": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		},
+		"long": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		}
+	},
+	"stress": {
+		"short": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		},
+		"long": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		}
+	},
+	"shame": {
+		"short": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		},
+		"long": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		}
+	},
+	"anger": {
+		"short": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		},
+		"long": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		}
+	},
+	"hopelessness": {
+		"short": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		},
+		"long": {
+			0: {
+			"title": "Meditation",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			1: {
+			"title": "Title 2",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			2: {
+			"title": "Title 3",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			3: {
+			"title": "Title 4",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+			4: {
+			"title": "Title 5",
+			"text": "Sit on the floor and close your eyes. Take a few deep breaths and focus on your breathing. When you feel your heart rate slow down, try to think of many good things in life."
+			},
+		}
+	},
+}
